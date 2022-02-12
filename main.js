662a2ce72f31308b2db00fd4c960cb4944940965
SaurabhK2347 <81204284+SaurabhK2347@users.noreply.github.com>
2022-02-12 13:27:47 -0600

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ Webcam.set({
     png_quality:90
 });
 
-camera = document.getElementById("cmaera");
+camera = document.getElementById("camera");
 
 Webcam.attach( '#camera' );
 
@@ -26,8 +26,39 @@ function modelLoaded() {
 function speak() 
 {
     var synth = window.speechSynthesis;
-    speak_data_1 = "The first prediction is " + prediction_1;
-    speak_data_2 = "And the second prediction is " + prediction_2;
-    var utterThis = new SpeechSynthesisisUtterance(speak_data_1 + speak_data_2);
+    speak_data = "The prediction is " + prediction;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
+}
+
+function check() 
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) 
+{
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML = results[0].label;
+        prediction = results[0].label;
+        speak();
+        if(results[0].label == "This is looking amazing") 
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+        if(results[0].label == "All the best") 
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if(results[0].label == "That was a marvelous victory") 
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#9996;";
+        }
+
+    
+    }
 }
